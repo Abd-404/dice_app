e818a7c2b7c050f488ff1154b6bd254bc026d27a
Abd-404 <abdullahmeraj243@gmail.com>
2023-09-25 21:03:48 +0300

    Initial commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/dice_roller.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.startColor, this.endColor, {super.key});
+  final Color startColor;
+  final Color endColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            startColor,
+            endColor,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
+
+class StyledText extends StatelessWidget {
+  const StyledText(this.text, {super.key});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(color: Colors.white, fontSize: 28),
+    );
+  }
+}
